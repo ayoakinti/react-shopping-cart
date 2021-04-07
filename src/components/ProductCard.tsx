145ee7afaf75af04cf708ac2d670452358048a61
Ayomide Akintimehin <ayoakinti@gmail.com>
2021-04-07 17:00:17 +0100

    Pushed over-product image

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,15 +1,46 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHeart,
+  faCartArrowDown,
+} from "@fortawesome/free-solid-svg-icons";
 import bag from "../assets/images/bag.png";
+import { useState } from "react";
 
 function ProductCard() {
+  const [showProductOptions, setShowProductOptions] = useState<boolean>(false);
+
   return (
     <div className="text-center">
-      <div className="product-image-container">
+      <div
+        className="product-image-container"
+        onMouseEnter={() => setShowProductOptions(true)}
+        onMouseLeave={() => setShowProductOptions(false)}
+      >
         <img src={bag} alt="bag" width="100%" height="100%" />
+        <div
+          className={`product-image-options ${
+            showProductOptions ? "show" : ""
+          }`}
+        >
+          <div className="d-flex justify-content-end" style={{ flex: "2" }}>
+            <ul className="d-flex flex-column justify-content-around p-1">
+              <li className="nav-icons">
+                <FontAwesomeIcon icon={faHeart} />
+              </li>
+              <li className="nav-icons">
+                <FontAwesomeIcon icon={faCartArrowDown} />
+              </li>
+            </ul>
+          </div>
+          <button className="w-100 btn btn-primary">Quick view</button>
+        </div>
       </div>
-      <p className="mt-1 mb-0">Category</p>
-      <h5 className="mt-1 mb-0">Modern Black House</h5>
-      <p className="mt-1 mb-0">Ratings</p>
-      <h5 className="mt-1 mb-0">$35.00</h5>
+      <div>
+        <p className="mt-1 mb-0">Category</p>
+        <h5 className="mt-1 mb-0">Modern Black House</h5>
+        <p className="mt-1 mb-0">Ratings</p>
+        <h5 className="mt-1 mb-0">$35.00</h5>
+      </div>
     </div>
   );
 }
--- src/components/ProductCardColumn.tsx
+++ src/components/ProductCardColumn.tsx
@@ -1,12 +1,37 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
 import bag from "../assets/images/bag.png";
+import { useState } from "react";
 
 function ProductCardColumn() {
+  const [showProductOptions, setShowProductOptions] = useState<boolean>(false);
   return (
     <div className="d-flex align-items-center mb-1">
-      <div className="product-image-column-container w-50">
+      <div
+        className="product-image-column-container w-50"
+        onMouseEnter={() => setShowProductOptions(true)}
+        onMouseLeave={() => setShowProductOptions(false)}
+      >
         <img src={bag} alt="bag" width="100%" height="100%" />
+        <div
+          className={`product-image-options ${
+            showProductOptions ? "show" : ""
+          }`}
+        >
+          <div className="d-flex justify-content-end" style={{ flex: "2" }}>
+            <ul className="d-flex flex-column justify-content-around p-1">
+              <li className="nav-icons">
+                <FontAwesomeIcon icon={faHeart} />
+              </li>
+              <li className="nav-icons">
+                <FontAwesomeIcon icon={faCartArrowDown} />
+              </li>
+            </ul>
+          </div>
+          <button className="w-100 btn btn-primary">Quick view</button>
+        </div>
       </div>
-      <div className='px-1'>
+      <div className="px-1">
         <p className="m-0">Category</p>
         <h5 className="mt-1 mb-0">Modern Black House</h5>
         <p className="mt-1 mb-0">Ratings</p>
