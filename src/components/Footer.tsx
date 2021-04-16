8d8a46e3a7aa865b7a87ddd8aef8ce1a148eca97
Ayomide Akintimehin <ayoakinti@gmail.com>
2021-04-16 10:56:38 +0100

    references added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
 import twitterIcon from "../assets/images/twitter.svg";
 import linkedInIcon from "../assets/images/linkedin.svg";
 import githubIcon from "../assets/images/github.svg";
@@ -36,85 +34,105 @@ function Footer() {
                 </ul>
               </div>
               <div className="col-md-3">
-                <h4>Customer Service</h4>
+                <h4>References</h4>
                 <ul>
-                  <li>Help & FAQs</li>
-                  <li>Order Lookup</li>
-                  <li>Shipping & Delivery</li>
-                  <li>Returns</li>
-                  <li>Contact Us</li>
+                  <li>
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://www.youtube.com/watch?v=sfmL6bGbiN8"
+                      className="nav-link"
+                    >
+                      Tutorial Motivation
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://themeforest.net/item/fixxo-ecommerce-xd-template/30467287"
+                      className="nav-link"
+                    >
+                      UI Guide
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://fakestoreapi.com/"
+                      className="nav-link"
+                    >
+                      API Guide
+                    </a>
+                  </li>
                 </ul>
               </div>
-              <div className="col-md-3 d-flex align-items-center">
-                <div>
-                  <h4>Ayo Akinti 😎</h4>
-                  <ul className="d-flex align-items-center">
-                    <li>
-                      <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href="https://ayoakinti.netlify.app/"
-                        className="nav-link"
-                      >
-                        <img
-                          src={logo}
-                          alt="portfolio"
-                          width="20px"
-                          height="20px"
-                        />
-                      </a>
-                    </li>
-                    <li className="ml-2">
-                      <a
-                        target="_blank"
-                        rel="noreferrer"
-                        className="nav-link"
-                        href="https://www.linkedin.com/in/ayomideakintimehin/"
-                      >
-                        <img
-                          src={linkedInIcon}
-                          alt="githubIcon"
-                          width="20px"
-                          height="20px"
-                        />
-                      </a>
-                    </li>
-                    <li className="ml-2">
-                      <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href="https://github.com/iamayoakinti"
-                        className="nav-link"
-                      >
-                        <img
-                          src={githubIcon}
-                          alt="githubIcon"
-                          width="20px"
-                          height="20px"
-                        />
-                      </a>
-                    </li>
-                    <li className="ml-2">
-                      <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href="https://twitter.com/iamayoakinti"
-                        className="nav-link"
-                      >
-                        <img
-                          src={twitterIcon}
-                          alt="twitterIcon"
-                          width="20px"
-                          height="20px"
-                        />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+              <div className="col-md-3">
+                <h4>My Profiles 😎</h4>
+                <ul className="d-flex align-items-center">
+                  <li>
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://ayoakinti.netlify.app/"
+                      className="nav-link"
+                    >
+                      <img
+                        src={logo}
+                        alt="portfolio"
+                        width="20px"
+                        height="20px"
+                      />
+                    </a>
+                  </li>
+                  <li className="ml-2">
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      className="nav-link"
+                      href="https://www.linkedin.com/in/ayomideakintimehin/"
+                    >
+                      <img
+                        src={linkedInIcon}
+                        alt="githubIcon"
+                        width="20px"
+                        height="20px"
+                      />
+                    </a>
+                  </li>
+                  <li className="ml-2">
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://github.com/iamayoakinti"
+                      className="nav-link"
+                    >
+                      <img
+                        src={githubIcon}
+                        alt="githubIcon"
+                        width="20px"
+                        height="20px"
+                      />
+                    </a>
+                  </li>
+                  <li className="ml-2">
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://twitter.com/iamayoakinti"
+                      className="nav-link"
+                    >
+                      <img
+                        src={twitterIcon}
+                        alt="twitterIcon"
+                        width="20px"
+                        height="20px"
+                      />
+                    </a>
+                  </li>
+                </ul>
               </div>
-              {/* <div className="col-md-3 d-flex align-items-center">
-                <div>Hi</div>
-              </div> */}
             </div>
           </div>
         </div>
